3e2c351267349fda67fd05dd0a81dff84866dd75
almir1904 <almir1904@users.noreply.github.com>
2024-11-27 01:57:32 +0100

    Update __init__.py

--- custom_components/shelly_custom/__init__.py
+++ custom_components/shelly_custom/__init__.py
@@ -1,16 +1,19 @@
 """The Shelly Custom integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 
-from .const import DOMAIN
-
-PLATFORMS = ["switch"]
+PLATFORMS = [Platform.SWITCH]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Shelly Custom from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unload_ok
